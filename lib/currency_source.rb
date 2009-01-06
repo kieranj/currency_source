0fb2e56899ee9680bef05bf85e61ce2585394506
Kieran Johnson <kieran@invisiblelines.com>
2009-01-06 13:00:33 +0000

    removed version include, update readme

--- lib/currency_source.rb
+++ lib/currency_source.rb
@@ -4,7 +4,7 @@ module CurrencySource
   require "hpricot"
   require "open-uri"
   
-  %w(currency currencies version).each do |lib|
+  %w(currency currencies).each do |lib|
      require File.join(File.dirname(__FILE__), 'currency_source', lib)
   end
   
